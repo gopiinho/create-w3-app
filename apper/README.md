cb50dae9e55e606ddb283bef3dfb1bbbe2f56c26
Gurpreet <gs109690@gmail.com>
2025-07-01 00:36:53 +0530

    🔥 Remove waste folder

--- apper/README.md
+++
@@ -1,3 +0,0 @@
-# W3 App
-
-Scaffold from [`create-w3-app`]("https://github.com/gopiinho/create-w3-app/")
--- apper/src/config/wagmi.ts
+++
@@ -1,9 +0,0 @@
-import { getDefaultConfig } from "@rainbow-me/rainbowkit";
-import { base, mainnet } from "wagmi/chains";
-
-export const wagmiConfig = getDefaultConfig({
-  appName: "Create Web3 Kit",
-  projectId: "YOUR_PROJECT_ID",
-  chains: [mainnet, base],
-  ssr: true,
-});
